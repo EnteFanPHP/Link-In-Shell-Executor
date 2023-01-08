9a40eeb836a78dfcd28285fb64bfcf7d436699a1
EnteFanPHP <78978307+EnteFanPHP@users.noreply.github.com>
2023-01-08 18:00:47 +0100

    Create link-executor.php

---
+++ link-executor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* @Author 'Khaled Daghstani'
+*
+* Valid urls:
+* Must start with https or http
+*/
+class LinkException extends \Exception {}
+
+function open_link(string $link) {
+    if (filter_var($link, FILTER_VALIDATE_URL)) {
+        if (PHP_OS == "Linux") {
+            $linux_cmd = "xdg-open";
+            $cmd = $linux_cmd;
+            if(command_exist_linux($linux_cmd)) {
+                exec($cmd." ".$link);
+            } else {
+                throw new LinkException("Command '".$cmd."' does not exists, install the command first");
+            }
+        }
+    } else {
+        throw new LinkException("Link is not valid");
+    }
+}
+
+function command_exist_linux($cmd) {
+    $return = shell_exec(sprintf("which %s", escapeshellarg($cmd)));
+    return !empty($return);
+}
